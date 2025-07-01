76cc6b0034da688a7e3189853fe40580880b3fea
junghongseop <a01082372487@gmail.com>
2025-07-01 09:02:50 +0900

    feat: 웹뷰 연결

---
+++ app/index.tsx
@@ -0,0 +1,59 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useRef } from "react";
+import { BackHandler, Platform, StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+const App = () => {
+  const webViewRef = useRef(null);
+  const canGoBack = useRef(false);
+
+  const onBackPress = () => {
+    if (canGoBack.current && webViewRef.current) {
+      (webViewRef.current as any).goBack();
+      return true;
+    }
+    return false;
+  };
+
+  React.useEffect(() => {
+    if (Platform.OS === "android") {
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        onBackPress
+      );
+      return () => backHandler.remove();
+    }
+  }, []);
+
+  const onNavigationStateChange = (navState: any) => {
+    canGoBack.current = navState.canGoBack;
+  };
+
+  return (
+    <View style={styles.container}>
+      <WebView
+        ref={webViewRef}
+        style={styles.webView}
+        source={{ uri: "http://10.150.2.232:3000" }}
+        allowsBackForwardNavigationGestures={true}
+        allowsFullscreenVideo={true}
+        bounces={false}
+        scrollEnabled={true}
+        showsVerticalScrollIndicator={false}
+        onNavigationStateChange={onNavigationStateChange}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webView: {
+    flex: 1,
+  },
+});
